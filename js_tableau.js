c1fd0ceb5af818251aa95f78147909241744bc38
Chichi <tatai@mail.com>
2024-12-24 14:50:21 +0100

    début codage du js

---
+++ js_tableau.js
@@ -0,0 +1,13 @@
+// var dupNode = first_line.cloneNode([deep]);
+
+let btn_ajouter = document.querySelector("#Ajouter")
+let btn_supprimer = document.querySelector("#supprimer")
+let btn_effacer = document.querySelector("#efffacer")
+console.log(btn_ajouter, btn_supprimer, btn_effacer)
+
+btn_ajouter.addEventListener("click", Addligne())
+
+function Addligne(){
+    console.log("click ajouter")
+    let ligne_a_dupliquer = 
+}
